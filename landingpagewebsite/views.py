3f3b6273c67a73393f39406fe7e771efd70557b1
YOUR_NAME <YOUR_EMAIL>
2022-05-18 21:54:45 +0300

    add new file

---
+++ landingpagewebsite/views.py
@@ -0,0 +1,12 @@
+from django.http.response import HttpResponse
+from django.shortcuts import render
+
+
+def first_page(request):
+    text = 'New text'
+    a = 'Hello world'
+    return render(request, './index.html',
+                  {
+                      'a': a,
+                      'text': text
+                  })
